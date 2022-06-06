5168e26e8b04ed0e7cea97272b3dddf038003acf
Zidane Innis <zidaneinnis@gmail.com>
2022-06-05 23:55:56 -0500

    Constructor Functions and the new Operator
    Constructor functions are not really a feature of Javascript, they are simply a pattern developed by other developers and others started using them.

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 'use strict';
 
-const numbers = [1, 2, 3];
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 
-const doubleNumArray = numbers.map(num => {
-  return num * 2;
-});
+  // Never create a method inside a constructor function (below)
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
 
-console.log(numbers);
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
 
-console.log(doubleNumArray);
+// 1. New empty object is created {}
+// 2. function is called, this = {}
+// 3. {} is linked to prototype
+// 4. function automatically returns {} from beginning
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+const jay = 'Jay';
+
+console.log(jonas instanceof Person);
+
+console.log(jay instanceof Person);
+
+// Constructor functions are not really a feature of Javascript, they are simply a pattern developed by other developers and others started using them.
